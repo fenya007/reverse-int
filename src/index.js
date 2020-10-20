e28b2c2413c3df9f4439b4660788e00ea5cd0489
fenya007 <59559796+fenya007@users.noreply.github.com>
2020-10-20 23:24:25 +0300

    add: task reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = (n<0?-1:1)*parseInt(n.toString().split('').reverse().join(''));
+    return res < 0 ? (res / -1) : res;
 }
+
